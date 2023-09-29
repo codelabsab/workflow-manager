6ea060b3d37767630c3d0376efae7f0c23054ee2
Carl-Fredrik Arvidson <carl-fredrik.arvidson@codelabs.se>
2023-09-29 16:06:17 +0200

    Adds a loading component to the history page (#62)

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="sm:mt-20sm:text-4xl mt-10 flex animate-pulse items-center justify-center text-xl font-semibold text-gray-500">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
